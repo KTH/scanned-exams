d794fa2023bb0877a2fad30b0dc39ee6364529c7
Carlos Saito <carlos@saito.style>
2021-03-30 16:51:30 +0200

    Create a fake API

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,29 @@
 const express = require("express");
 const path = require("path");
+const apiRouter = require("./api/router")
 
 const PORT = 4000;
+const server = express();
 
-const app = express();
-
-app.use(
+// Routes:
+// - /           The welcome page
+// - /api        API routes (only for authorized people)
+// - /app        The React application (including all frontend artifacts
+//               like CSS files)
+// - /auth       routes for the authorization process
+// - /_monitor   just the monitor page
+server.get("/", (req, res) => {
+  res.status(200).send("Yay");
+});
+server.get("/api", apiRouter)
+server.use(
   "/app",
   express.static(path.join(__dirname, "..", "frontend", "build"))
 );
+server.get("/_monitor", (req, res) => {
+  res.send("APPLICATION_STATUS: OK")
+})
 
-app.get("/", (req, res) => {
-  res.status(200).send("Yay");
-});
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
