f491db21f6c93a088302ae44c6fd2ab74f0739f0
Carlos Saito <carlos@saito.style>
2021-03-31 15:17:32 +0200

    Mocked routes

--- tentaapi-mock/app.js
+++ tentaapi-mock/app.js
@@ -3,6 +3,20 @@ const express = require("express");
 const server = express();
 log.init.pino();
 
+server.get("/Ladok/activity/:ladokId", (req, res) => {
+  log.info(req.params.ladokId);
+  res.send("Return information from Aktivitetstillfälle from Ladok");
+});
+
+server.post("/windream/search/documents/false", (req, res) => {
+  res.send("Get the list of exams given search parameters");
+});
+
+server.get("/windream/file/:fileId/true", (req, res) => {
+  log.info(req.params.fileId);
+  res.send("Get a scanned PDF file");
+});
+
 server.listen(4001, (req, res) => {
   log.info("Started tentaapi-mock in port 4001.");
   log.fatal("YOU SHOULD ALWAYS RUN THIS APP LOCALLY");
