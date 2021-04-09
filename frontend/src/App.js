b77733bf73840b35e623ec689d9e9c8597f7e258
Johan Öhman Saldes <johan.saldes@gmail.com>
2021-04-09 13:57:52 +0200

    check if assignment exist and create assignment option

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CreateAssignment from "./components/CreateAssignment";
 import UploadScannedExams from "./components/UploadScannedExams";
+import { createAssignment, getAssignment } from "./utils";
 function App() {
   const [created, setCreated] = useState(false);
-  const onCreate = () => {
-    // Api call...
-    setCreated(true);
+
+  useEffect(() => {
+    const init = async () => {
+      const res = await getAssignment();
+      const data = await res.json();
+      if (data.assignment) {
+        setCreated(true);
+      }
+    };
+    init();
+  }, [setCreated]);
+
+  const onCreate = async () => {
+    const res = await createAssignment();
+    if (res.ok) {
+      setCreated(true);
+    }
   };
 
   if (created) {
