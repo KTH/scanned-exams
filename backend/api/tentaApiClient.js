314c63b68132339520b9a8f55f9a708c5c2967c5
Johan Öhman Saldes <johan.saldes@gmail.com>
2021-05-05 13:58:40 +0200

    try/catch tentaApiClient

--- backend/api/tentaApiClient.js
+++ backend/api/tentaApiClient.js
@@ -8,82 +8,94 @@ const client = got.extend({
 
 /** Get the courseCode, examCode, examDate given the Ladok UID */
 async function getExamination(ladokId) {
-  const { body } = await client(`Ladok/activity/${ladokId}`, {
-    responseType: "json",
-  });
+  try {
+    const { body } = await client(`Ladok/activity/${ladokId}`, {
+      responseType: "json",
+    });
 
-  const allCodes = body.kopplingar.map((k) => ({
-    examCode: k.aktivitet.utbildningskod,
-    courseCode: k.kursinstans.utbildningskod,
-  }));
+    const allCodes = body.kopplingar.map((k) => ({
+      examCode: k.aktivitet.utbildningskod,
+      courseCode: k.kursinstans.utbildningskod,
+    }));
 
-  return {
-    courseCode: allCodes[0].courseCode,
-    examCode: allCodes[0].examCode,
-    examDate: body.datumperiod.startdatum,
-  };
+    return {
+      courseCode: allCodes[0].courseCode,
+      examCode: allCodes[0].examCode,
+      examDate: body.datumperiod.startdatum,
+    };
+  } catch (err) {
+    log.error(err);
+  }
 }
 
 /** Get a list of all exam files for a given exam */
 async function examList({ courseCode, examDate, examCode }) {
-  log.info(`Getting exams for ${courseCode} ${examDate} ${examCode}`);
-  const { body } = await client("windream/search/documents/false", {
-    method: "POST",
-    json: {
-      searchIndiceses: [
-        {
-          index: "c_code",
-          value: courseCode,
-          useWildcard: false,
-        },
-        {
-          index: "e_code",
-          value: examCode,
-          useWildcard: false,
-        },
-        {
-          index: "e_date",
-          value: examDate,
-          useWildcard: false,
-        },
-      ],
-      includeDocumentIndicesesInResponse: true,
-      includeSystemIndicesesInResponse: false,
-      useDatesInSearch: false,
-    },
-    responseType: "json",
-  });
+  try {
+    log.info(`Getting exams for ${courseCode} ${examDate} ${examCode}`);
+    const { body } = await client("windream/search/documents/false", {
+      method: "POST",
+      json: {
+        searchIndiceses: [
+          {
+            index: "c_code",
+            value: courseCode,
+            useWildcard: false,
+          },
+          {
+            index: "e_code",
+            value: examCode,
+            useWildcard: false,
+          },
+          {
+            index: "e_date",
+            value: examDate,
+            useWildcard: false,
+          },
+        ],
+        includeDocumentIndicesesInResponse: true,
+        includeSystemIndicesesInResponse: false,
+        useDatesInSearch: false,
+      },
+      responseType: "json",
+    });
 
-  const list = [];
+    const list = [];
 
-  for (const result of body.documentSearchResults) {
-    const keyValue = result.documentIndiceses.find(
-      (di) => di.index === "s_uid"
-    );
+    for (const result of body.documentSearchResults) {
+      const keyValue = result.documentIndiceses.find(
+        (di) => di.index === "s_uid"
+      );
 
-    if (keyValue && keyValue.value) {
-      list.push({
-        fileId: result.id,
-        userId: keyValue.value,
-      });
+      if (keyValue && keyValue.value) {
+        list.push({
+          fileId: result.id,
+          userId: keyValue.value,
+        });
+      }
     }
-  }
 
-  return list;
+    return list;
+  } catch (err) {
+    log.error(err);
+  }
 }
 
 /** Download the exam with ID "fileId" to the given "filePath" */
 async function downloadExam(fileId, filePath) {
-  log.info(`Downloading file ${fileId}...`);
-  const { body } = await client(`windream/file/${fileId}/true`, {
-    responseType: "json",
-  });
+  try {
+    log.info(`Downloading file ${fileId}...`);
+    const { body } = await client(`windream/file/${fileId}/true`, {
+      responseType: "json",
+    });
 
-  const download = Buffer.from(
-    body.wdFile.fileAsBase64.toString("utf-8"),
-    "base64"
-  );
-  fs.writeFileSync(filePath, download);
+    const download = Buffer.from(
+      body.wdFile.fileAsBase64.toString("utf-8"),
+      "base64"
+    );
+    fs.writeFileSync(filePath, download);
+  } catch (err) {
+    log.error(err);
+  }
 }
 
 module.exports = {
