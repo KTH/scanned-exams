df0973cf14766bd3bdba5badb3e02f7addd0877e
Johan Öhman Saldes <johan.saldes@gmail.com>
2021-05-05 15:01:13 +0200

    added try/catch to api router

--- backend/api/router.js
+++ backend/api/router.js
@@ -1,59 +1,76 @@
 const express = require("express");
 const canvas = require("./canvasApiClient");
 const transferExams = require("./transferExams.js");
+const log = require("skog");
+const { internalServerError, unauthorized } = require("../utils");
 
 const router = express.Router();
 
 router.use(function checkAuthorization(req, res, next) {
   if (!req.session.courseId || !req.session.userId) {
-    return res.status(401).json({
-      message: "Unauthorized",
-    });
+    return unauthorized(`Missing userId (unauthorized): ${userId}`, res);
   }
 
   next();
 });
 
 router.get("/assignment", async (req, res) => {
-  const assignment = await canvas.getValidAssignment(req.session.courseId);
+  try {
+    const assignment = await canvas.getValidAssignment(req.session.courseId);
 
-  res.json({
-    assignment,
-  });
+    res.json({
+      assignment,
+    });
+  } catch (err) {
+    log.error(err);
+    internalServerError(err, res);
+  }
 });
 
 router.post("/assignment", async (req, res) => {
-  let assignment = await canvas.getValidAssignment(req.session.courseId);
+  try {
+    let assignment = await canvas.getValidAssignment(req.session.courseId);
 
-  if (!assignment) {
-    assignment = await canvas.createAssignment(
-      req.session.courseId,
-      req.session.examination
-    );
+    if (!assignment) {
+      assignment = await canvas.createAssignment(
+        req.session.courseId,
+        req.session.examination
+      );
+    }
+
+    res.json({
+      message: "Assignment created successfully",
+      assignment,
+    });
+  } catch (err) {
+    internalServerError(err, res);
   }
-
-  res.json({
-    message: "Assignment created successfully",
-    assignment,
-  });
 });
 
 router.post("/exams", (req, res) => {
-  transferExams(req.session);
+  try {
+    transferExams(req.session);
 
-  res.json({
-    message: "Exam uploading started",
-  });
+    res.json({
+      message: "Exam uploading started",
+    });
+  } catch (err) {
+    internalServerError(err, res);
+  }
 });
 
 router.get("/exams", (req, res) => {
-  const session = req.session;
+  try {
+    const session = req.session;
 
-  // TODO: return a different code depending on the error
-  res.status(session.error ? 400 : 200).json({
-    state: session.state,
-    error: session.error,
-  });
+    // TODO: return a different code depending on the error
+    res.status(session.error ? 400 : 200).json({
+      state: session.state,
+      error: session.error,
+    });
+  } catch (err) {
+    internalServerError(err, res);
+  }
 });
 
 module.exports = router;
