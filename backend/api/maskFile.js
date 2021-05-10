166f6025282f3fc2e927d77b9c31e67dd74e2bc9
Emil Stenberg <esandin@gmail.com>
2021-05-10 11:06:37 +0200

    added todo:s

--- backend/api/maskFile.js
+++ backend/api/maskFile.js
@@ -1,13 +1,24 @@
 const gm = require("gm");
 const path = require("path");
 const fs = require("fs");
+// TODO: remove rimraf, use fsPromises.rmdir instead
 const rimraf = require("rimraf");
 const os = require("os");
 const PDFDocument = require("pdfkit");
 
+// TODO: this has to be load tested. In the test course, my pretty beefy computer used around 5% cpu when masking the exams.
+// What if a user in a big course with 1000:s of exams, each around 100 MB, clicks multiple times in a row? Will our docker servers be able to manage that?
+//
+
+// TODO: we should test if this blocks the process. If you click the 'upload' button, is the app still responsive? Or will other users not be able to reach the app while it masks the exams?
+
 function getPages(file) {
+  // TODO: comment or refactor to make this code more understandable
+  // TODO: crash with an obvious error if ImageMagick or GraphicsMagick insn't installed
   return new Promise((resolve) => {
+    // TODO: what does this line do? Add a comment.
     gm(file).identify("%p ", (err, data) => {
+      //TODO: what are these 10 and 1 magic numbers? Name them.
       resolve(data.split(" ").map((d) => parseInt(d, 10) - 1));
     });
   });
@@ -21,6 +32,7 @@ async function convertToPdf(inputs, output) {
   doc.pipe(fs.createWriteStream(output));
 
   for (const input of inputs) {
+    // TODO: what are these 595,842 magic numbers? Name them.
     doc.addPage({ size: [595, 842], margin: 0 });
     doc.image(input, { fit: [595, 842], align: "center", valign: "center" });
   }
@@ -35,6 +47,7 @@ function maskImage(input, output) {
       .compress("jpeg")
       .fill("#fff")
       .stroke("#9dc2ea", 1)
+      // These numbers should be named to make it easier to understand
       .drawRectangle(560, 240 - 70, 560 + 240, 240 + 70)
       .write(output, function (err) {
         if (err) {
@@ -47,11 +60,13 @@ function maskImage(input, output) {
 }
 
 module.exports = async function maskFile(input, output) {
+  // TODO: can't have sync calls, that will block the app from all other requests. Should be change to await fsPromises.mkdtemp without sync.
   const tmp = fs.mkdtempSync(path.join(os.tmpdir(), "masked-images"));
 
   const pages = await getPages(input);
   const maskedImages = [];
 
+  // Why do we slice? Add a comment.
   for (const page of pages.slice(1)) {
     const maskedImage = path.resolve(tmp, `${page}.png`);
 
@@ -60,5 +75,6 @@ module.exports = async function maskFile(input, output) {
   }
   await convertToPdf(maskedImages, output);
   console.log("DONE", output);
+  // TODO: remove rimraf, use fsPromises.rmdir instead
   rimraf.sync(tmp);
 };
