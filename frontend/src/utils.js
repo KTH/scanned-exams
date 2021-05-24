e82400ebcedf3514261d05fb9274e3e86efff24a
Carlos Saito <carlos@saito.style>
2021-05-24 15:41:10 +0200

    Send `courseId` in every request

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,28 +1,36 @@
+const courseId = window.COURSE_ID
+
 export const getAssignment = async () =>
-  await fetch("/scanned-exams/api/assignment", {
+  await fetch(`/scanned-exams/api/assignment?courseId=${courseId}`, {
     headers: {
       Authorization: `Bearer ${process.env.REACT_APP_CANVAS_API_ADMIN_TOKEN}`,
     },
   });
 
 export const createAssignment = async () =>
-  await fetch("/scanned-exams/api/assignment", {
+  await fetch(`/scanned-exams/api/assignment`, {
     method: "POST",
     headers: {
       Authorization: `Bearer ${process.env.REACT_APP_CANVAS_API_ADMIN_TOKEN}`,
+    },
+    body: {
+      courseId,
     },
   });
 
 export const sendExam = async () =>
-  await fetch("/scanned-exams/api/exams", {
+  await fetch(`/scanned-exams/api/exams`, {
     method: "POST",
     headers: {
       Authorization: `Bearer ${process.env.REACT_APP_CANVAS_API_ADMIN_TOKEN}`,
     },
+    body: {
+      courseId,
+    }
   });
 
 export const uploadStatus = async () =>
-  await fetch("/scanned-exams/api/exams", {
+  await fetch(`/scanned-exams/api/exams?courseId=${courseId}`, {
     method: "GET",
     headers: {
       Authorization: `Bearer ${process.env.REACT_APP_CANVAS_API_ADMIN_TOKEN}`,
