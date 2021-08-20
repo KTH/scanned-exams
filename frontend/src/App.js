a5e7159eac332fb7b583c5bba796c25cffc4b231
Carlos Saito <carlos@saito.style>
2021-08-20 15:52:34 +0200

    Check if the user is logged in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,46 @@
 import React from "react";
-import UploadScreen from "./screens/Upload";
+import { useQuery, QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
+
+async function getUserData() {
+  const response = await window.fetch("/scanned-exams/api/me");
+
+  if (response.status === 404) {
+    return null;
+  }
+
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+
+  return response.json();
+}
+
+function App2() {
+  const query = useQuery("user", getUserData);
+
+  if (query.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (query.isError) {
+    throw query.error;
+  }
+
+  if (query.data) {
+    return <div>You are logged in</div>;
+  }
+
+  return <div>You are not logged in</div>;
+}
 
 export default function App() {
   return (
-    <div className="container mx-auto">
-      <UploadScreen />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="container mx-auto">
+        <App2 />
+      </div>
+    </QueryClientProvider>
   );
 }
