ba2408a818f792f1bafd0ac31c676ddc9fe1dca1
Carlos Saito <carlos@saito.style>
2021-08-23 11:12:19 +0200

    Clean up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,8 @@ import { getUserData } from "./utils/apiClient";
 import Welcome from "./screens/Welcome";
 import AuthenticatedApp from "./screens/AuthenticatedApp";
 
-/*
-function getCourseId() {
-  const urlParams = new URLSearchParams(window.location.search);
-
-  return urlParams.get("courseId");
-}
-*/
-
 export default function App() {
   const query = useQuery("user", getUserData);
-  // const courseId = getCourseId();
 
   if (query.isLoading) {
     return <div>Loading...</div>;
--- frontend/src/utils/apiClient.js
+++ frontend/src/utils/apiClient.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/prefer-default-export */
 export async function getUserData() {
   const response = await window.fetch("/scanned-exams/api/me");
 
@@ -7,17 +8,6 @@ export async function getUserData() {
 
   if (!response.ok) {
     throw new Error("Network response was not ok");
-  }
-
-  return response.json();
-}
-
-export async function getCourse({ queryKey }) {
-  const [, courseId] = queryKey;
-  const response = await window.fetch(`/scanned-exams/api/courses/${courseId}`);
-
-  if (!response.ok) {
-    throw new Error("Something wrong happened");
   }
 
   return response.json();
