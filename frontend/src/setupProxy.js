db678db9bd6897484b0fd9ec1dd6f94b3b45ee73
Carlos Saito <carlos@saito.style>
2021-08-23 09:31:55 +0200

    Document setupProxy.js

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,6 +1,11 @@
+/**
+ * This file is used to setup the development server
+ *
+ * It proxies requests from /api, etc -> https://localdev.kth.se:4444
+ * Read more: Read: https://create-react-app.dev/docs/proxying-api-requests-in-development#configuring-the-proxy-manually
+ */
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-// Read: https://create-react-app.dev/docs/proxying-api-requests-in-development#configuring-the-proxy-manually
 module.exports = function setupProxy(app) {
   const proxy = createProxyMiddleware({
     target: "https://localdev.kth.se:4444",
