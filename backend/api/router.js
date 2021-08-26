1d350d07611ce8e1fb19384afa61ef306285a043
Carlos Saito <carlos@saito.style>
2021-08-26 08:14:20 +0200

    Remove unnecesary async

--- backend/api/router.js
+++ backend/api/router.js
@@ -7,7 +7,7 @@ const router = express.Router();
  * Returns data from the logged in user.
  * - Returns a 404 if the user is not logged in
  */
-router.get("/me", async (req, res) => {
+router.get("/me", (req, res) => {
   const { userId } = req.session;
 
   if (!userId) {
