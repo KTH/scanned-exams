db4cbc4fdab5cbf3b95a4c601a09e86fac2f205d
jhsware <sebastian@urbantalk.se>
2021-09-03 17:21:29 +0200

    Some comments and a precursor to general client ApiErrors

--- backend/api/error.js
+++ backend/api/error.js
@@ -40,3 +40,34 @@ module.exports = {
   errorHandler,
   EndpointSpecificError,
 };
+
+/* ************************************************
+
+OTHER POSSIBLE GENERAL ERROR TYPES
+
+Using typed errors makes it easy to search for them and allows
+piping and other techniques to separate code that handles them.
+
+//  These errors should be thrown by the service api client:
+
+class EndpointAuthError extends Error {
+  // Auth issues with service
+}
+
+class EndpointNetworkError extends Error {
+  // Network issues connecting to service
+}
+
+//  This could be handled by the service api client or the endpoint
+
+class EndpointValueError extends Error {
+  // Wrongs or invalid params sent
+}
+
+// This is handled globally for all endpoints
+
+class EndpointUnhandledError extends Error {
+  // Errors that aren't handled by above
+}
+
+*/
