68364e8e01edb616872ff73ae62b1cbebe208b2f
Carlos Saito <carlos@saito.style>
2021-09-09 12:03:22 +0200

    Fix. Check if session exists

--- backend/api/utils.js
+++ backend/api/utils.js
@@ -4,23 +4,30 @@ const canvas = require("./canvasApiClient");
 async function checkAuthorization(req, res, next) {
   const { id: courseId } = req.params;
   const { userId } = req.session;
-  const { roles, authorized } = await canvas.getAuthorizationData(
-    courseId,
-    userId
-  );
 
-  if (authorized) {
-    log.debug(`Authorized. User ${userId} in Course ${courseId}.`);
+  if (courseId && userId) {
+    const { roles, authorized } = await canvas.getAuthorizationData(
+      courseId,
+      userId
+    );
 
-    return next();
+    if (authorized) {
+      log.debug(`Authorized. User ${userId} in Course ${courseId}.`);
+
+      return next();
+    }
+
+    log.warn(
+      `Not authorized. User ${userId} in Course ${courseId} has roles: [${roles}].`
+    );
+
+    return res.status(401).send({
+      message: "Unauthorized: you must be teacher or examiner to use this app",
+    });
   }
 
-  log.warn(
-    `Not authorized. User ${userId} in Course ${courseId} has roles: [${roles}].`
-  );
-
-  return res.status(401).send({
-    message: "Unauthorized: you must be teacher or examiner to use this app",
+  return res.status(400).send({
+    message: "No courseId provided or no session available",
   });
 }
 
