d50974016df4896cf5555a739d60967cbbb5bbd0
jhsware <sebastian@urbantalk.se>
2021-09-09 12:39:19 +0200

    Added config for JEST with in memory mongodb

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -4,4 +4,8 @@ If you need to customise JEST config, be aware that:
 
 - @shelf/jest-mongodb "If you have a custom jest.config.js make sure you remove testEnvironment property, otherwise it will conflict with the preset."
   https://github.com/shelfio/jest-mongodb
-*/ 
+*/
+
+module.exports = {
+  preset: "@shelf/jest-mongodb",
+};
