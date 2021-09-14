0695c060c5c54d0ebe9949081fd06983a47c9ee6
Carlos Saito <carlos@saito.style>
2021-09-14 11:18:26 +0200

    Manual revert of modification in frontend

--- frontend/src/features/setup-course/steps/CreateHomePage.js
+++ frontend/src/features/setup-course/steps/CreateHomePage.js
@@ -9,30 +9,15 @@ export default function CreateHomePage({ onCreate, skip, courseId }) {
     },
   });
 
-  const { mutate, isLoading, isSuccess, isError, error } = mutation;
+  const { mutate, isLoading, isSuccess, isError } = mutation;
+
+  if (isError) {
+    throw mutation.error;
+  }
 
   return (
     <div className="max-w-2xl">
       <H2>Prepare the homepage</H2>
-      {!isError &&
-        renderContent({
-          isLoading,
-          isSuccess,
-          onAction: () => mutate(),
-          onSkip: () => skip(),
-        })}
-      {isError &&
-        renderError({
-          message: error.message,
-          onReset: () => mutation.reset(),
-        })}
-    </div>
-  );
-}
-
-function renderContent({ isLoading, isSuccess, onAction, onSkip }) {
-  return (
-    <>
       <P>
         The exam room will be visible for your students, it is important that
         they can see its purpose from the homepage. You can use our recommended
@@ -44,29 +29,16 @@ function renderContent({ isLoading, isSuccess, onAction, onSkip }) {
       <div className="mt-8">
         <PrimaryButton
           className="sm:w-96"
-          onClick={onAction}
+          onClick={mutate}
           waiting={isLoading}
           success={isSuccess}
         >
           Use the recommended homepage
         </PrimaryButton>
-        <SecondaryButton className="sm:w-auto" onClick={onSkip}>
+        <SecondaryButton className="sm:w-auto" onClick={skip}>
           Skip this step
         </SecondaryButton>
       </div>
-    </> /* end frag */
-  );
-}
-
-function renderError({ message, onReset }) {
-  return (
-    <>
-      <P>{message}</P>
-      <div className="mt-8">
-        <SecondaryButton className="sm:w-auto" onClick={onReset}>
-          Try Again!
-        </SecondaryButton>
-      </div>
-    </> /* end frag */
+    </div>
   );
 }
