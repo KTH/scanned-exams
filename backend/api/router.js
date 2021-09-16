ebb6e2216074646056df7fa826309745d248f7fd
jhsware <sebastian@urbantalk.se>
2021-09-16 09:41:59 +0200

    Improvements to endpoint that starts import

--- backend/api/router.js
+++ backend/api/router.js
@@ -121,33 +121,51 @@ router.get("/courses/:id/import/status", async (req, res) => {
 });
 
 // Start the import process
-router.post("/courses/:id/import/start", async (req, res) => {
-  const courseId = req.params.id;
-  const { status } = await getStatusFromQueue(courseId);
+router.post(
+  "/courses/:id/import/start",
+  express.json(),
+  async (req, res, next) => {
+    const courseId = req.params.id;
+    const { status } = await getStatusFromQueue(courseId);
 
-  if (status !== "idle") {
-    throw new EndpointError({
-      type: "queue_not_idle",
-      message: "Can't start import if there are pending jobs",
-      statusCode: 409, // Conflict - Indicates that the request could not be processed because of conflict in the current state of the resource
-    });
+    if (!Array.isArray(req.body)) {
+      return next(
+        new EndpointError({
+          type: "missing_body",
+          message: "This endpoint expects to get a list of fileIds to import",
+          statusCode: 400, // Bad Request
+        })
+      );
+    }
+
+    if (status !== "idle") {
+      return next(
+        new EndpointError({
+          type: "queue_not_idle",
+          message:
+            "Can't start new import if the queue for this course is working",
+          statusCode: 409, // Conflict - Indicates that the request could not be processed because of conflict in the current state of the resource
+        })
+      );
+    }
+
+    await removeFinishedEntries(courseId);
+
+    for (const fileId of req.body) {
+      // eslint-disable-next-line no-await-in-loop
+      await addEntryToQueue({
+        fileId,
+        courseId,
+        status: "pending",
+      });
+    }
+
+    // Return the queue status object so stats can be updated
+    // in frontend
+    const statusObj = await getStatusFromQueue(courseId);
+    return res.status(200).send(statusObj);
   }
-
-  await removeFinishedEntries(courseId);
-
-  for (const fileId of req.body) {
-    // eslint-disable-next-line no-await-in-loop
-    await addEntryToQueue({
-      fileId,
-      courseId,
-      status: "pending",
-    });
-  }
-
-  return res.status(200).send({
-    message: "done",
-  });
-});
+);
 
 router.use(errorHandler);
 module.exports = router;
