e1a4d00661b981751e4509130273ca56df0fb63e
jhsware <sebastian@urbantalk.se>
2021-09-21 15:05:08 +0200

    Improved presentation of application errors

--- frontend/src/common/FullPageError.js
+++ frontend/src/common/FullPageError.js
@@ -1,9 +1,17 @@
 import React from "react";
 
+const errorTitles = {
+  permission_denied: "Apologies! Permission denied",
+};
+
 export default function FullPageError({ error }) {
+  const title = errorTitles[error.type] || "Oops! Trouble Encountered";
   return (
-    <div role="alert" className="bg-red-100 p-12">
-      {error.message}
+    <div className="flex flex-row items-center justify-center">
+      <div role="alert" className="bg-red-100 p-12 m-12 max-w-3xl">
+        <h2 className="text-xl font-semibold tracking-tight">{title}</h2>
+        <p className="my-6 text-lg">{error.message}</p>
+      </div>
     </div>
   );
 }
