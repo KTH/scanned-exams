5178da63110144c4dafc2eba9f3529bd135dd4b2
Carlos Saito <carlos@saito.style>
2021-09-22 14:39:30 +0200

    Format

--- backend/api/router.js
+++ backend/api/router.js
@@ -123,66 +123,63 @@ router.get("/courses/:id/import/status", async (req, res) => {
 });
 
 // Start the import process
-router.post(
-  "/courses/:id/import/start",
-  async (req, res, next) => {
-    const courseId = req.params.id;
-    const { status } = await getStatusFromQueue(courseId);
+router.post("/courses/:id/import/start", async (req, res, next) => {
+  const courseId = req.params.id;
+  const { status } = await getStatusFromQueue(courseId);
 
-    if (!Array.isArray(req.body)) {
-      return next(
-        new EndpointError({
-          type: "missing_body",
-          message: "This endpoint expects to get a list of fileIds to import",
-          statusCode: 400, // Bad Request
-        })
-      );
-    }
-
-    if (status !== "idle") {
-      return next(
-        new EndpointError({
-          type: "queue_not_idle",
-          message:
-            "Can't start new import if the queue for this course is working",
-          statusCode: 409, // Conflict - Indicates that the request could not be processed because of conflict in the current state of the resource
-        })
-      );
-    }
-
-    await resetQueueForImport(courseId);
-
-    for (const fileId of req.body) {
-      // eslint-disable-next-line no-await-in-loop
-      await addEntryToQueue({
-        fileId,
-        courseId,
-        status: "pending",
+  if (!Array.isArray(req.body)) {
+    return next(
+      new EndpointError({
+        type: "missing_body",
+        message: "This endpoint expects to get a list of fileIds to import",
+        statusCode: 400, // Bad Request
       })
-        // eslint-disable-next-line no-await-in-loop
-        .catch(async (err) => {
-          if (
-            err.message.startsWith(
-              "Add to queue failed becuase entry exist for this fileId"
-            )
-          ) {
-            // We get an error if it already exists so setting it to pending
-            await updateStatusOfEntryInQueue(
-              {
-                fileId,
-              },
-              "pending"
-            );
-          }
-        });
-    }
-
-    // Return the queue status object so stats can be updated
-    // in frontend
-    const statusObj = await getStatusFromQueue(courseId);
-    return res.status(200).send(statusObj);
+    );
   }
-);
+
+  if (status !== "idle") {
+    return next(
+      new EndpointError({
+        type: "queue_not_idle",
+        message:
+          "Can't start new import if the queue for this course is working",
+        statusCode: 409, // Conflict - Indicates that the request could not be processed because of conflict in the current state of the resource
+      })
+    );
+  }
+
+  await resetQueueForImport(courseId);
+
+  for (const fileId of req.body) {
+    // eslint-disable-next-line no-await-in-loop
+    await addEntryToQueue({
+      fileId,
+      courseId,
+      status: "pending",
+    })
+      // eslint-disable-next-line no-await-in-loop
+      .catch(async (err) => {
+        if (
+          err.message.startsWith(
+            "Add to queue failed becuase entry exist for this fileId"
+          )
+        ) {
+          // We get an error if it already exists so setting it to pending
+          await updateStatusOfEntryInQueue(
+            {
+              fileId,
+            },
+            "pending"
+          );
+        }
+      });
+  }
+
+  // Return the queue status object so stats can be updated
+  // in frontend
+  const statusObj = await getStatusFromQueue(courseId);
+  return res.status(200).send(statusObj);
+});
 
 router.use(errorHandler);
 module.exports = router;
