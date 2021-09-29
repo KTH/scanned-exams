59c5430484b730b2a5feb686a11497ddc28ba010
Carlos Saito <carlos@saito.style>
2021-09-29 16:43:12 +0200

    Wip: good looking checkbox

--- frontend/src/features/setup-course/steps/SetupCourse.js
+++ frontend/src/features/setup-course/steps/SetupCourse.js
@@ -1,5 +1,5 @@
-/* eslint-disable prettier/prettier */
-import React from "react";
+/* eslint-disable react/no-unescaped-entities */
+import React, { useState } from "react";
 import { useMutateCourseSetup } from "../../../common/api";
 import { P, H2, PrimaryButton } from "../../widgets";
 
@@ -10,29 +10,49 @@ export default function SetupCourse({ courseId }) {
   );
   const setHomepageMutation = useMutateCourseSetup(courseId, "create-homepage");
 
+  const [recommendedHomepage, setRecommendedHomepage] = useState(true);
+
   return (
-    <div>
-      <H2>Setup course</H2>
-      <P>
-        This examroom will be published and therefore visible for students. Do
-        you want us to set a homepage for you or do you want to change it
-        yourself?
-      </P>
-      <P>
-        <input type="radio" name="create-homepage" value="yes" />
-        <span>Use recommended homepage</span>
+    <div className="max-w-2xl">
+      <H2>Setup examroom</H2>
+      <P>Choose one option to setup your examroom</P>
+      <div className="my-8">
+        <label
+          htmlFor="create-homepage-yes"
+          className="flex border rounded px-6 py-4 my-4 border-blue-500"
+        >
+          <input
+            type="radio"
+            id="create-homepage-yes"
+            className="my-1 mr-2"
+            checked
+          />
+          <div className="">
+            <h3 className="font-semibold">Recommended setup</h3>
+            <ul className="list-disc my-2 ml-6">
+              <li className="p-1">Create a special assignment called "Scanned Exams"</li>
+              <li className="p-1">Replace the existing homepage with a template</li>
+            </ul>
+          </div>
+        </label>
 
-        <input type="radio" name="create-homepage" value="no" />
-        <span>No</span>
+        <label
+          htmlFor="create-homepage-yes"
+          className="flex border rounded px-6 py-4 my-4 border-gray-200"
+        >
+          <input type="radio" id="create-homepage-no" className="my-1 mr-2" />
+          <div className="">
+            <h3 className="font-semibold">Minimal setup</h3>
+            <ul className="list-disc my-2 ml-6">
+              <li className="p-1">Only create a special assignment called "Scanned Exams"</li>
+            </ul>
+          </div>
+        </label>
+      </div>
+      <P>
+        Note: neither the examroom or assignment will be published in this step
       </P>
-
-      <P>When you click the button below:</P>
-      <ul>
-        <li>Homepage will be replaced with the recommended page</li>
-        <li>A special assignment called &quot;Scanned exams&quot; will be created</li>
-      </ul>
-      <P>Neither the examroom or assignment will be published in this step</P>
-      <PrimaryButton>Setup courseroom</PrimaryButton>
+      <PrimaryButton>Setup examroom</PrimaryButton>
     </div>
   );
 }
