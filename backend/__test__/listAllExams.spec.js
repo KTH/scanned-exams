e2e15bf95626d1bc4d58a32e5dc0987d9b518f59
Emil Stenberg <esandin@gmail.com>
2021-10-01 14:27:14 +0200

    added stub for mocking ladok och tentaapi

--- backend/__test__/listAllExams.spec.js
+++ backend/__test__/listAllExams.spec.js
@@ -4,7 +4,21 @@ const listAllExams = rewire("../api/listAllExams");
 
 describe("list scanned exams", () => {
   const tentaApi = listAllExams.__get__("tentaApi");
+  const ladok = listAllExams.__get__('ladok')
+  
   it("should... ", async () => {
-    expect(typedEntry.status).toBe("new");
+    // Setup mock
+    tentaApi.examListByLadokId = function(){
+      console.log('::::::::: Mocked tentaApi function ::::::::::::::')
+    }
+
+    ladok.getAktivitetstillfalle = function(){
+      console.log('::::::::::::::: Mocked ladok function :::::::::::::')
+    }
+
+    const courseId = 'SF1624'
+    const ladokID = '123123123123'
+    listAllExams.listScannedExams(courseId, ladokID)
+    // expect(typedEntry.status).toBe("new");
   });
 });
