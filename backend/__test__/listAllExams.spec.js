93c9bd9719199c37c1ce9b561c9f5a51262681b1
Emil Stenberg <esandin@gmail.com>
2021-10-04 12:03:08 +0200

    rename test

--- backend/__test__/listAllExams.spec.js
+++ backend/__test__/listAllExams.spec.js
@@ -1,6 +1,6 @@
 const { expect } = require("@jest/globals");
 const listAllExams = require("../api/listAllExams");
-describe("List all exams", () => {
+describe("mergeAndDeduplicate", () => {
   it("should include exams with old format, exams with new format, but no duplicates", () => {
     const examsWithNewFormat = [{ fileId: "1" }, { fileId: "2" }];
     const examsWithOldFormat = [{ fileId: "1" }, { fileId: "3" }];
