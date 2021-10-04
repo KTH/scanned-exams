52b32bb27dc34bc1e8df385c11c431c8827e9f6d
Emil Stenberg <esandin@gmail.com>
2021-10-04 10:17:06 +0200

    test passes

--- backend/__test__/listAllExams.spec.js
+++ backend/__test__/listAllExams.spec.js
@@ -3,22 +3,30 @@ const rewire = require("rewire");
 const listAllExams = rewire("../api/listAllExams");
 
 describe("list scanned exams", () => {
-  // const tentaApi = listAllExams.__get__("tentaApi");
-  // const ladok = listAllExams.__get__('ladok')
+  const tentaApi = listAllExams.__get__("tentaApi");
+  const ladok = listAllExams.__get__('ladok')
+
+  // TODO: should reset the mocked functions, otherwise they might mess with other tests
 
   it("should... ", async () => {
+    const fileId = '1'
+    const fileId2 = '2'
     // Setup mock
-    // tentaApi.examListByLadokId = function(){
-    //   console.log('::::::::: Mocked tentaApi function ::::::::::::::')
-    // }
+    // TODO: would prefer if we had a listScannedExamsWithNewFormat, that would allow me to design the test without knowing the internal logic
+    tentaApi.examListByLadokId = function(){
+      console.log('::::::::: Mocked tentaApi function ::::::::::::::')
+      return [{fileId},{fileId: fileId2}]
+    }
 
-    // ladok.getAktivitetstillfalle = function(){
-    //   console.log('::::::::::::::: Mocked ladok function :::::::::::::')
-    // }
+    listAllExams.__set__('listScannedExamsWithOldFormat',function(){
+      console.log('::::::::::::::: Mocked listScannedExamsWithOldFormat function :::::::::::::')
 
-    // const courseId = 'SF1624'
-    // const ladokID = '123123123123'
-    // listAllExams.listScannedExams(courseId, ladokID)
-    expect(1).toBe(1);
+      return [{fileId}]
+    })
+
+    const courseId = 'SF1624'
+    const ladokID = '123123123123'
+    const result = await listAllExams.listScannedExams(courseId, ladokID)
+    expect(result).toEqual([{fileId},{fileId:fileId2}]);
   });
 });
