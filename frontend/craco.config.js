3d626114d894ea139e11cfeeb70fa495ce0be211
jhsware <sebastian@urbantalk.se>
2021-11-04 14:32:40 +0100

    Move proxy settings to craco.config.js

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  devServer: {
+    allowedHosts: ["localdev.kth.se"],
+    host: "localdev.kth.se",
+    port: 4443,
+    https: true,
+    proxy: {
+      "/scanned-exams/api": "http://localhost:4444",
+      "/scanned-exams/auth": "http://localhost:4444",
+    },
+  },
   style: {
     postcss: {
       // eslint-disable-next-line import/no-extraneous-dependencies
