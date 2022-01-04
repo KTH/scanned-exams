27acdcfb721528fe571d9b77614b97ac270f8fdd
jhsware <sebastian@urbantalk.se>
2022-01-04 09:26:47 +0100

    Update intro text

--- frontend/src/features/unauthenticated-app/UnauthenticatedApp.tsx
+++ frontend/src/features/unauthenticated-app/UnauthenticatedApp.tsx
@@ -4,12 +4,12 @@ export default function UnauthenticatedApp({ courseId }: any) {
   return (
     <div className="max-w-screen-lg">
       <div className="text-4xl font-semibold mt-8 mb-4 tracking-tight">
-        Scanned Exams
+        KTH Import Exams
       </div>
       <p className="my-6 text-xl">
-        <b>Scanned Exams</b> imports scanned paper exams to Canvas allowing you
-        to grade them using SpeedGrader. The app is maintained by the E-learning
-        team at KTH.
+        <b>KTH Import Exams</b> imports scanned paper exams to Canvas allowing you
+        to grade them using SpeedGrader. The application is maintained by the E-learning
+        team at KTH. Please read more about <a href="https://intra.kth.se/en/utbildning/examination/salskrivning/skannade/tentor-till-canvas-1.1085000">the application here</a>.
       </p>
       <p className="my-6">
         This app requires you to have a <b>teacher role</b> in this exam room.
