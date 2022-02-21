a775391d579b4d1020c6efc95e403a9ad043e4d6
Carlos Saito <carlos@saito.style>
2022-02-21 14:49:23 +0100

    Rever some changes

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -8,12 +8,12 @@ log.init.pino({
 });
 
 process.on("uncaughtException", (err) => {
-  log.fatal(err, `Uncaught exception: ${err.message}`);
+  log.fatal(err, `Reject ${err}`);
   process.exit(1);
 });
 
 process.on("unhandledRejection", (reason) => {
-  log.fatal(reason, `Unhandled promise rejection: ${reason}`);
+  log.fatal(reason, `Reject: ${reason}`);
   process.exit(1);
 });
 
