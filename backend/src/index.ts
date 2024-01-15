c478adbdb74757eb02bc2f739c5debd8f7320258
Carlos Saito <carlos@saito.style>
2024-01-15 17:10:28 +0100

    Add HTTPS proxy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,11 @@
-import "./app-https";
+import "./config";
+import log from "skog";
+import server from "./server";
+import { startBackgroundImport } from "./importWorker";
+
+const PORT = process.env.PORT || 3000;
+
+startBackgroundImport();
+server.listen(PORT, () => {
+  log.info(`App listening on port ${PORT}`);
+});
