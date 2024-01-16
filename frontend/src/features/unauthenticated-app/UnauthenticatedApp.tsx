98fff5e3497b11be7aba5264a6531838f442b158
Carlos Saito <carlos@saito.style>
2024-01-16 15:37:49 +0100

    Style UnauthenticatedApp component

--- frontend/src/features/unauthenticated-app/UnauthenticatedApp.tsx
+++ frontend/src/features/unauthenticated-app/UnauthenticatedApp.tsx
@@ -1,32 +1,35 @@
 import React from "react";
+import "./UnauthenticatedApp.scss";
 
 export default function UnauthenticatedApp({ courseId }: any) {
   return (
-    <div className="max-w-screen-lg">
-      <div className="text-4xl font-semibold mt-8 mb-4 tracking-tight">
-        KTH Import Exams
-      </div>
-      <p className="my-6 text-xl">
-      If you have conducted a written exam on paper and want to use SpeedGrader for the assessment,
-      you can import the scanned exams to Canvas by using this app. Read more 
-      about <a target="_blank" className="text-blue-500 no-underline hover:text-blue-700 hover:underline" href="https://intra.kth.se/en/utbildning/examination/salskrivning/skannade/tentor-till-canvas-1.1085000">KTH Import Exams
-      on the KTH Intranet</a>.
+    <div className="container">
+      <h1>KTH Import Exams</h1>
+      <p>
+        If you have conducted a written exam on paper and want to use
+        SpeedGrader for the assessment, you can import the scanned exams to
+        Canvas by using this app. Read more about{" "}
+        <a
+          target="_blank"
+          className=""
+          href="https://intra.kth.se/en/utbildning/examination/salskrivning/skannade/tentor-till-canvas-1.1085000"
+        >
+          KTH Import Exams on the KTH Intranet.
+        </a>
       </p>
-      <p className="my-6">
+      <p>
         This app requires you to have a <b>teacher role</b> in this exam room.
       </p>
-      <p className="my-6">
-      Please make sure that the exam administration is made aware well in advance of the exams being 
-      assessed in Canvas and not on paper, since this impacts the administrative process. If you have 
-      any questions, please contact <a className="text-blue-500 no-underline hover:text-blue-700 hover:underline" href="mailto:it-support@kth.se">it-support@kth.se</a>!
+      <p>
+        Please make sure that the exam administration is made aware well in
+        advance of the exams being assessed in Canvas and not on paper, since
+        this impacts the administrative process. If you have any questions,
+        please contact <a href="mailto:it-support@kth.se">it-support@kth.se</a>!
       </p>
-      <div className="bg-blue-100 p-6 my-12 flex flex-row items-center justify-center">
+      <div className="">
         <form method="post" action="/scanned-exams/auth">
           <input name="courseId" type="hidden" value={courseId} />
-          <button
-            type="submit"
-            className="bg-blue-500 text-white rounded-md font-semibold w-56 py-2 hover:bg-blue-700 transition-colors"
-          >
+          <button type="submit" className="kth-button primary">
             Launch app
           </button>
         </form>
